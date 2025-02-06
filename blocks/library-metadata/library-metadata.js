72639ad91f0de0df3a7d3ee52fbc0d1470ab2013
Elaine Sun <lansun@adobe.com>
2025-02-06 00:37:38 -0800

    making it library-metadata work...

--- blocks/library-metadata/library-metadata.js
+++ blocks/library-metadata/library-metadata.js
@@ -1,5 +1,4 @@
 export default async (block) => {
-  // const { default: init } = await import('https://milo.adobe.com/libs/blocks/library-metadata/library-metadata.js');
-  const { default: init } = await import('https://main--random-ai-stuff--elainesun.aem.page/libs/blocks/library-metadata/library-metadata.js');
+  const { default: init } = await import('https://milo.adobe.com/libs/blocks/library-metadata/library-metadata.js');
   init(block);
 };
